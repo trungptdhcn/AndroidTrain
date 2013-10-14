565e9787f9667ce6ed7c783114b80f9bb252aa0b
Phạm Trần Trung <trungptdhcn@gmail.com>
2013-10-14 10:39:44 +0700

    update423423

--- OnlineDio/src/com/example/OnlineDio/MyActivity.java
+++ OnlineDio/src/com/example/OnlineDio/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.OnlineDio;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,19 +18,39 @@ public class MyActivity extends Activity
     private EditText login_edtEmail;
     private EditText login_edtPass;
     private TextView login_tvForgotPass;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        login_btDone = (Button)findViewById(R.id.login_btDone);
-        login_btBack  = (Button)findViewById(R.id.login_btBack);
-//        login_edtEmail = (EditText)findViewById(R.id.login_edtEmail);
-//        login_edtPass = (EditText)findViewById(R.id.login_edtPass);
+        login_btDone = (Button) findViewById(R.id.login_btDone);
+        login_btBack = (Button) findViewById(R.id.login_btBack);
+        login_edtEmail = (EditText) findViewById(R.id.login_edtEmail);
+        login_edtPass = (EditText) findViewById(R.id.login_edtPass);
+
+        login_btDone.setOnClickListener(new ClickListener());
+
 
     }
+
     public void validateAccount()
     {
 
     }
+    class ClickListener implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v)
+        {
+            switch (v.getId())
+            {
+                case R.id.login_btDone:
+                    Intent i = new Intent(MyActivity.this,HomeActivity.class);
+                    startActivity(i);
+            }
+        }
+    }
 }
+
