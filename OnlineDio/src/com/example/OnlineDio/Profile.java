851e92b1d6f03f737c48f1ae16b22d67c12fa373
Phạm Trần Trung <trungptdhcn@gmail.com>
2013-10-16 17:55:26 +0700

    update

---
+++ OnlineDio/src/com/example/OnlineDio/Profile.java
@@ -0,0 +1,21 @@
+package com.example.OnlineDio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trung
+ * Date: 16/10/2013
+ * Time: 17:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Profile extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
+        setContentView(R.layout.profile);
+    }
+}
